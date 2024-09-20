0f21ca4e66b4942bf2e2e3a8035407c2362885b9
abhijithp327 <87142203+abhijithp327@users.noreply.github.com>
2024-09-20 11:00:55 +0530

    added branch model

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -12,18 +12,90 @@ const userSchema = new mongoose.Schema({
         enum: ['Customer', 'Admin', 'DeliveryPartner'],
         required: true
     },
+    isActivated: {
+        type: Boolean,
+        default: false
+    }
+});
+
+// Custom Schema
+const customerSchema = new mongoose.Schema({
+    ...userSchema.obj,
+    phone: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    role: {
+        type: String,
+        enum: ['Customer'],
+        default: 'Customer'
+    },
+    liveLocation: {
+        latitude: { type: Number },
+        longitude: { type: Number },
+    },
+    address: {
+        type: String
+    }
+});
+
+// Delivery Partner Schema
+const deliveryPartnerSchema = new mongoose.Schema({
+    ...userSchema.obj,
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
         required: true
     },
-    isAdmin: {
-        type: Boolean,
-        default: false
-    }
-}, {
-    timestamps: true
+    phone: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    role: {
+        type: String,
+        enum: ['DeliveryPartner'],
+        default: 'DeliveryPartner'
+    },
+    liveLocation: {
+        latitude: { type: Number },
+        longitude: { type: Number },
+    },
+    address: {
+        type: String
+    },
+    branch: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Branch'
+    },
 });
+
+// Admin Schema 
+const adminSchema = new mongoose.Schema({
+    ...userSchema.obj,
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['Admin'],
+        default: 'Admin'
+    },
+});
+
+
+
+export const Customer = mongoose.model('Customer', customerSchema);
+export const DeliveryPartner = mongoose.model('DeliveryPartner', deliveryPartnerSchema);
+export const Admin = mongoose.model('Admin', adminSchema);
